99abb4809e72d28955b2b23e32ea4d8550b50549
Jiří Vrba <jiri@vrba.dev>
2020-12-20 21:39:20 +0100

    Disabled warning for missing goon

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,6 @@ use Mix.Config
 config :nostrum,
   num_shards: :auto,
   token: System.get_env("DISCORD_TOKEN")
+
+# Disable startup warning about missing Goon executable
+config :porcelain, goon_warn_if_missing: false
