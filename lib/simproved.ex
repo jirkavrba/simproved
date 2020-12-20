3b245c97a2cceb87d55f427305f863f0b3496c3b
Jiří Vrba <jiri@vrba.dev>
2020-12-20 21:40:44 +0100

    Removed application scaffolding as I am using escripts

--- lib/simproved.ex
+++ lib/simproved.ex
@@ -2,13 +2,8 @@ defmodule Simproved do
   @moduledoc """
   Simproved application entry point, basically just loads the environment and starts the consumer
   """
-  use Application
 
-  @impl true
-  def start(_type, _arguments) do
-    children = [Simproved.Supervisor]
-    options = [strategy: :rest_for_one, name: Simproved.Supervisor]
-
-    Supervisor.start_link(children, options)
+  def main(_arguments) do
+    Simproved.Supervisor.start_link(name: Simproved.Consumer)
   end
 end
